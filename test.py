70ed8cb8b0e3a160de630b4c64e14fdf24086c5c
midonado@davidson.edu <55297780+midonado@users.noreply.github.com>
2022-04-06 14:30:31 -0400

    five v0.1

--- test.py
+++ test.py
@@ -83,10 +83,10 @@ if __name__ == '__main__':
         while True:
             for ch in timeDiff:
                 for digit in digits:
-                    GPIO.outpit(digit, 0)
+                    GPIO.output(digit, 0)
                     for loop in range(0, 7):
                         GPIO.output(segments[loop], not num[ch][loop])
-                    GPIO.outpit(digit, 1)
+                    GPIO.output(digit, 1)
 
             if(input() == "q"): break
     finally:
