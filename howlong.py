1b34fbd23f42c292267b9efef02c7ed7ecc84686
midonado@davidson.edu <55297780+midonado@users.noreply.github.com>
2022-05-04 15:32:35 -0400

    updates

--- howlong.py
+++ howlong.py
@@ -1,40 +1,11 @@
 import datetime
 import time
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
 
 # 5 PM Constant
 FIVE_PM = datetime.timedelta(
     hours=17,
     minutes=0
 )
-
-segments = (17, 16, 11, 26, 22, 13, 24)
-num = {' ': (1, 1, 1, 1, 1, 1, 1),
-       '0': (0, 0, 0, 0, 0, 0, 1),
-       '1': (1, 0, 0, 1, 1, 1, 1),
-       '2': (0, 0, 1, 0, 0, 1, 0),
-       '3': (0, 0, 0, 0, 1, 1, 0),
-       '4': (1, 0, 0, 1, 1, 0, 0),
-       '5': (0, 1, 0, 0, 1, 0, 0),
-       '6': (0, 1, 0, 0, 0, 0, 0),
-       '7': (0, 0, 0, 1, 1, 1, 1),
-       '8': (0, 0, 0, 0, 0, 0, 0),
-       '9': (0, 0, 0, 0, 1, 0, 0),
-       'u': (1, 1, 0, 0, 0, 1, 1),
-       'n': (1, 1, 0, 1, 0, 1, 0),
-       't': (1, 1, 1, 0, 0, 0, 0),
-       'i': (1, 1, 0, 1, 1, 1, 1)}
-
-
-# TODO: cleanup comments/documentation
-
-def setup():
-    for segment in segments:
-        GPIO.setup(segment, GPIO.OUT)
-        GPIO.output(segment, 1)
-
 
 def getTime():  # returns current time as datetime with hours + mins
     currentTime = time.localtime()
@@ -47,7 +18,7 @@ def getTime():  # returns current time as datetime with hours + mins
 
 
 def stringTime(hours, mins):  # stringifies time
-    return str(hours).rjust(2, "0") + \
+    return str(hours).rjust(2, "0") + ":" + \
         str(mins).rjust(2, "0")
 
 
@@ -64,20 +35,7 @@ def getTimeDiff():
     time = getTime()
     return timeDiff(time)
 
-# TODO: process string into GPIO output
-
-
 if __name__ == '__main__':
-    setup()
-    try:
-        timeDiff = getTimeDiff()
-        timeDiff += "unti15"
-        print(timeDiff)
-        while True:
-            for ch in timeDiff:
-                for loop in range(0, 7):
-                    GPIO.output(segments[loop], num[ch][loop])
-                time.sleep(0.3)
-            break
-    finally:
-        GPIO.cleanup()
+    timeDiff = getTimeDiff()
+    timeDiff += " until 5"
+    print(timeDiff)
